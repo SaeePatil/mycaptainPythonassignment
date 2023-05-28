ef757ab53b645a96add8a0e395eebc837d690b84
SaeePatil <134829816+SaeePatil@users.noreply.github.com>
2023-05-28 11:56:33 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 28 11:24:59 2023
+
+@author: hp
+"""
+
+x=float(input('Enter the radius of the circle:'))
+Area=x*x*3.142
+print('the area of the circle of given radius is:',Area)
