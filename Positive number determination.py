45d90f7dfe5dbcbd0db487cf5ff4e3463afa115b
SaeePatil <134829816+SaeePatil@users.noreply.github.com>
2023-05-29 22:25:43 +0530

    Add files via upload

---
+++ Positive number determination.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 29 22:15:14 2023
+
+@author: hp
+"""
+
+n=int(input('Enter a number:'))
+if n>=0:
+    print('the number is positive')
+    else:
+        print('the number is negative')
+        
+    
+    
